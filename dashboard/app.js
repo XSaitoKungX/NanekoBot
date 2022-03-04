f8190c7055bf334cff8ceaca8aa2af5cfc6b09ed
XSaitoKungX <markungphim@gmail.com>
2022-03-04 02:21:40 +0100

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -39,7 +39,7 @@ module.exports.load = async(client) => {
 		.use(async function(req, res, next){
 			req.user = req.session.user;
 			req.client = client;
-			req.locale = req.user ? (req.user.locale === "fr" ? "fr-FR" : "en-US") : "en-US";
+			req.locale = req.user ? (req.user.locale === "de" ? "de-DE" : "en-US") : "en-US";
 			if(req.user && req.url !== "/") req.userInfos = await utils.fetchUser(req.user, req.client);
 			if(req.user){
 				req.translate = req.client.translations.get(req.locale);
